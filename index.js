93c0fb78956eafd84355e6bb1e5379727ef6da0c
Christian Richard <lifepupil@gmail.com>
2015-04-30 12:21:30 -0700

    added comments to explain code

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var Hoek = require('hoek');
 var Server = require('./lib/server');
 
 Server.init(8000, function(err, server){
+	// this blows up if there's an error
+	// this callback function is called by the
+	// return next(err, server) statement
+	// The assertion is a kind of claim (or hope) that
+	// the server is NOT blowing up.
 	Hoek.assert(!err, err);
 	console.log('Hapi is listening', server.info.uri);
 });
